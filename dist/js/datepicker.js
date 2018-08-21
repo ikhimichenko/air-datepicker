78d83bbc4503baddc8450ff72eed2d3f85b28331
ikhimichenko <sstrykerr@gmail.com>
2018-08-21 17:44:24 +0300

    add en locale

--- dist/js/datepicker.js
+++ dist/js/datepicker.js
@@ -1483,6 +1483,18 @@
             dateFormat: 'dd.mm.yyyy',
             timeFormat: 'hh:ii',
             firstDay: 1
+        },
+        en: {
+            days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+            daysShort: ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'],
+            daysMin: ['Su','Mo','Tu','We','Th','Fr','Sa'],
+            months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+            monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+            today: 'Today',
+            clear: 'Clear',
+            dateFormat: 'yyyy-mm-dd',
+            timeFormat: 'hh:ii',
+            firstDay: 1
         }
     };
 
